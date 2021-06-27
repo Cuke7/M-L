831ba347a28465dca4760bdf3730452d6111a29f
Cuke7 <cuke.dev@mail.com>
2021-06-27 13:15:41 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Tinder key-name="id" :queue.sync="queue" :offset-y="10" @submit="onSubmit">
+            <template slot-scope="scope">
+                <div
+                    class="pic"
+                    :style="{
+                        'background-image': `url(${scope.data.id})`,
+                    }"
+                />
+            </template>
+            <img class="like-pointer" slot="like" src="./assets/like-txt.png" />
+            <img class="nope-pointer" slot="nope" src="./assets/nope-txt.png" />
+            <img class="super-pointer" slot="super" src="./assets/super-txt.png" />
+        </Tinder>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tinder from "vue-tinder";
+import source from "@/bing2";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: { Tinder },
+    data: () => ({
+        queue: [],
+        offset: 0,
+    }),
+    created() {
+        this.mock();
+    },
+    methods: {
+        mock(count = 5) {
+            const list = [];
+            for (let i = 0; i < count; i++) {
+                list.push({ id: source[this.offset] });
+                this.offset++;
+            }
+            this.queue = this.queue.concat(list);
+        },
+        onSubmit({ item }) {
+            // if (this.queue.length < 3) {
+            //     this.mock();
+            // }
+        },
+    },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+    height: 100%;
+}
+
+body {
+    margin: 0;
+    background-color: #20262e;
+    overflow: hidden;
+}
+
+#app .vue-tinder {
+    position: absolute;
+    z-index: 1;
+    left: 0;
+    right: 0;
+    top: 23px;
+    bottom: 46px;
+    margin: auto;
+    min-width: 300px;
+    max-width: 355px;
+}
+
+.nope-pointer,
+.like-pointer {
+    position: absolute;
+    z-index: 1;
+    top: 20px;
+    width: 64px;
+    height: 64px;
+}
+
+.nope-pointer {
+    right: 10px;
+}
+
+.like-pointer {
+    left: 10px;
+}
+
+.super-pointer {
+    position: absolute;
+    z-index: 1;
+    bottom: 80px;
+    left: 0;
+    right: 0;
+    margin: auto;
+    width: 112px;
+    height: 78px;
+}
+
+.pic {
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-position: center;
 }
 </style>
---
+++ src/bing2.js
@@ -0,0 +1,7 @@
+export default [
+    "https://cdn.gemperles.com/media/catalog/product/U/p/UpFiles_Image_2010_11_05_634245404279716242_2.jpg",
+    "https://www.histoiredor.com/dw/image/v2/BCQS_PRD/on/demandware.static/-/Sites-THOM_CATALOG/default/dwc4814f68/images/B3DFBZW271-master.jpg?sw=268&sh=268",
+    "https://cdn.gemmyo.com/skin/frontend/gemmyo/default/images/cms/choisir-bdf/lady2.jpg",
+    "https://www.mariage.be/images/conseils-mariage/photo-mariage/bague-de-fiancailles.jpg",
+    "https://cdn.shopify.com/s/files/1/0039/6040/6080/products/ELEONORE_05_800x.jpg?v=1617133719",
+];
